d8fb5df873a3e6ea37d4a488f3fea803a4bfd9f5
ncinsli <56553344+ncinsli@users.noreply.github.com>
2022-09-06 23:56:26 +0500

    Initial commit (empty project)

---
+++ Anisel/Anisel/AniselApp.swift
@@ -0,0 +1,17 @@
+//
+//  AniselApp.swift
+//  Anisel
+//
+//  Created by Василий Гайсен on 06.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct AniselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
